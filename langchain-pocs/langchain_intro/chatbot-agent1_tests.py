78878a7ad5d0f23c0cfbf1464e22a6871de90d39
nivaldomonteiro <nushnivaldo@gmail.com>
2025-05-02 22:09:49 +0200

    Added agents tests

---
+++ langchain-pocs/langchain_intro/chatbot-agent1_tests.py
@@ -0,0 +1,9 @@
+from chatbotagent1 import hospital_agent_executor
+
+hospital_agent_executor.invoke(
+    {"input":"What is the current wait time at hospital C?"}
+)
+
+hospital_agent_executor.invoke(
+     {"input": "What have patients said about their comfort at the hospital?"}
+)
